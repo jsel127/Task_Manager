fb8d0733cfa45956f32cce20964df5c5826781f1
Jasmine <jsel4635@gmail.com>
2024-09-18 09:38:08 -0700

    Finshed index.html for tasks to be able to show all tasks created by a specific user.

--- tasks/models.py
+++ tasks/models.py
@@ -3,10 +3,12 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Task(models.Model):
-    name = models.CharField(max_length=60)
+    name = models.CharField(max_length=60, blank=False)
     description = models.CharField(max_length=1000)
     start_date = models.DateField()
-    end_date = models.DateField()
+    end_date = models.DateField(blank=False)
     completed = models.BooleanField(default=False)
     deleted = models.BooleanField(default=False)
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="tasks")
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="tasks", blank=False)
+    def __str__(self):
+        return f"{self.name}"
--- tasks/views.py
+++ tasks/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, "tasks/index.html")
+    if request.user.is_authenticated:
+        return render(request, "tasks/index.html", {
+            "tasks": request.user.tasks.all()
+        })
 
 # def add(request):
 
 # def task(request, task_id):
+
