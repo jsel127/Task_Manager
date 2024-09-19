adeba3968d4f0060163bc3ef4d750b62ea155b25
Jasmine <jsel4635@gmail.com>
2024-09-19 07:50:54 -0700

    Got following functionality working for task app: adding new task, viewing tasks (linked each task to own view with all details), only show tasks of logged in user

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = "tasks"
 urlpatterns = [
     path('', views.index, name="index"),
-    # path('add', views.add , name="add"),
-    # path('task/<int:task_id>', views.task, name="task")
+    path('add', views.add , name="add"),
+    path('task/<int:task_id>', views.task, name="task")
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from .models import Task
+from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
+class AddTaskForm(forms.Form):
+    name = forms.CharField(max_length=60)
+    description = forms.CharField(max_length=1000)
+    start_date = forms.DateField()
+    end_date = forms.DateField()
+    
 # Create your views here.
 def index(request):
     if request.user.is_authenticated:
@@ -7,7 +17,25 @@ def index(request):
             "tasks": request.user.tasks.all()
         })
 
-# def add(request):
+def add(request):
+    if request.method == "POST":
+        task = AddTaskForm(request.POST)
+        if task.is_valid():
+            name = task.cleaned_data["name"]
+            description = task.cleaned_data["description"]
+            start_date = task.cleaned_data["start_date"]
+            end_date = task.cleaned_data["end_date"]
+            Task.objects.create(name=name, description=description, start_date=start_date, end_date=end_date, created_by=request.user)
+            return HttpResponseRedirect(reverse('tasks:index'))
+        else:
+            return render(request, 'tasks/add.html', {
+                "form": task
+            })
+    return render(request, "tasks/add.html", {
+        "form": AddTaskForm()
+    })
 
-# def task(request, task_id):
-
+def task(request, task_id):
+    return render(request, "tasks/task.html", {
+        "task": Task.objects.get(pk=task_id)
+    })
