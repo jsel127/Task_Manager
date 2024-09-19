a6cb4a6271c154f418ea4f9fbd2e315f6d9890ea
Jasmine <jsel4635@gmail.com>
2024-09-19 09:56:34 -0700

    Added delete functionality to tasks. Error in updating tasks where discard and save do the same thing and adds a new task instead of updating current task.

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,5 +5,7 @@ app_name = "tasks"
 urlpatterns = [
     path('', views.index, name="index"),
     path('add', views.add , name="add"),
-    path('task/<int:task_id>', views.task, name="task")
+    path('task/<int:task_id>', views.task, name="task"),
+    path('task/<int:task_id>/delete', views.delete, name="delete"),
+    path('task/<int:task_id>/update', views.update, name="update"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -4,7 +4,7 @@ from django import forms
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
-class AddTaskForm(forms.Form):
+class TaskForm(forms.Form):
     name = forms.CharField(max_length=60)
     description = forms.CharField(max_length=1000)
     start_date = forms.DateField()
@@ -14,12 +14,12 @@ class AddTaskForm(forms.Form):
 def index(request):
     if request.user.is_authenticated:
         return render(request, "tasks/index.html", {
-            "tasks": request.user.tasks.all()
+            "tasks": request.user.tasks.exclude(deleted=True).exclude(completed=True).all()
         })
 
 def add(request):
     if request.method == "POST":
-        task = AddTaskForm(request.POST)
+        task = TaskForm(request.POST)
         if task.is_valid():
             name = task.cleaned_data["name"]
             description = task.cleaned_data["description"]
@@ -32,10 +32,37 @@ def add(request):
                 "form": task
             })
     return render(request, "tasks/add.html", {
-        "form": AddTaskForm()
+        "form": TaskForm()
     })
 
 def task(request, task_id):
     return render(request, "tasks/task.html", {
         "task": Task.objects.get(pk=task_id)
     })
+
+def update(request, task_id):
+    task = Task.objects.get(pk=task_id)
+    if request.method == "POST":
+        updated_task = TaskForm(request.POST)
+        if updated_task.is_valid():
+            if 'save' in request.POST:
+                task.name = updated_task.cleaned_data["name"]
+                task.description = updated_task.cleaned_data["description"]
+                task.start_date = updated_task.cleaned_data["start_date"]
+                task.end_date = updated_task.cleaned_data["end_date"]
+                task.save()
+            return HttpResponseRedirect(reverse('tasks:task', arg=(task.id,)))
+    return render(request, "tasks/update.html", {
+        "form": TaskForm(initial={
+            "name": task.name,
+            "description": task.description,
+            "start_date": task.start_date, 
+            "end_date": task.end_date
+        })
+    })
+
+def delete(request, task_id):
+    task = Task.objects.get(pk=task_id)
+    task.deleted = True
+    task.save()
+    return HttpResponseRedirect(reverse('tasks:index'))
